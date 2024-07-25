231c4fad6c345065dd7eef4322f49d5f914c67e8
lqr131115 <1046975211@qq.com>
2024-07-25 23:16:11 +0800

    refac: bin/www

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const Koa = require("koa");
-const router = require("./router/index");
-const koa = new Koa();
-
-router.prefix('/mock');
-koa.use(router.routes());
-koa.listen(3001, () => {
-  console.log("server is running at http://localhost:3001");
-});
